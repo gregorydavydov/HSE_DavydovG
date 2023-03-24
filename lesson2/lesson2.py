6ac23f8ef9764e2dcae9a3110b64b3cefc852a56
gregorydavydov <davydovgrand@gmail.com>
2023-03-25 00:06:07 +0300

    into

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -3,10 +3,20 @@ from lesson_2_data import courts, repondents
 enumarate - нумерация выводимых данных
 range () – это служебная функция для создания списка чисел. Список сгенерированных чисел полезен для логики итераций. 
 """
+
+
+#Task two
 def main():
-    count = 0
-    for i, repondent in enumerate(repondents):
-        print(i, repondent["inn"])
+    for repondent in repondents:
+        if 'case_number' in repondent:
+            name = repondent['short_name']
+            inn = repondent['inn']
+            ogrn = repondent['ogrn']
+            address = repondent['address']
+            case_number = repondent['case_number']
+            print("Истец: Иванов Иван Иванович""\n""ИНН: 1236182357", "ОГРНИП: 218431927812733""\n""Адрес: 123534, г. Москва, ул. Водников, 13""\n\n"f"Ответчик: {name}""\n"f"ИНН: {inn}, ОГРН: {ogrn}""\n"f"Адрес: {address}""\n\n"f"Номер дела: {case_number}""\n\n")
+
+
 
 if __name__ == "__main__":
     main()
