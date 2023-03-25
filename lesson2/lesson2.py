90f6da0cf20cafe6b3e66e356d0dc347b6961939
gregorydavydov <davydovgrand@gmail.com>
2023-03-25 14:48:08 +0300

    into

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -9,12 +9,25 @@ range () – это служебная функция для создания с
 def main():
     for repondent in repondents:
         if 'case_number' in repondent:
+            case_number = repondent['case_number']
+            court_code = case_number.split('-')[0]
+            if court_code in courts:
+                repondent['court_name'] = courts['court_code']and['court_name']
+            else:
+                repondent['court_name'] = 'Неизвестный суд'
             name = repondent['short_name']
             inn = repondent['inn']
             ogrn = repondent['ogrn']
             address = repondent['address']
             case_number = repondent['case_number']
-            print("Истец: Иванов Иван Иванович""\n""ИНН: 1236182357", "ОГРНИП: 218431927812733""\n""Адрес: 123534, г. Москва, ул. Водников, 13""\n\n"f"Ответчик: {name}""\n"f"ИНН: {inn}, ОГРН: {ogrn}""\n"f"Адрес: {address}""\n\n"f"Номер дела: {case_number}""\n\n")
+            print(f"Код: {court_code}")
+            print("Истец: Иванов Иван Иванович""\n""ИНН: 1236182357", "ОГРНИП: 218431927812733""\n""Адрес: 123534, г. Москва, ул. Водников, 13""\n\n"f"Ответчик: {name}""\n"f"ИНН: {inn}, ОГРН: {ogrn}""\n"f"Адрес: {address}""\n"f"Номер дела: {case_number}""\n\n")
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
