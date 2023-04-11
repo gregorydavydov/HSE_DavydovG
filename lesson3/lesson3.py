fd0c9b9d837aed41950849a52e8694fea8fab96a
gregorydavydov <davydovgrand@gmail.com>
2023-04-11 20:41:19 +0300

    into

--- lesson3/lesson3.py
+++ lesson3/lesson3.py
@@ -5,107 +5,42 @@ next(f) - также открыть очередную строку
 content = f.readlines() - прочитать файл целиком в назначенную переменную 'content'
 enumerate() - перечисление выведенных значений
 line.strip('\n').split(',') = убираем лишние символы + преобразуем значения в список
-
-f = open('traders.txt')
-b = open('traders.json')
-for line in f
-line = line.strip('\n').split(',')
-content = line
-print(line or content)
-
-break
-
-with open ('traders.txt') as file:
-    for line in file:
-        line = line.strip('\n').split(',')
-        content = line
-        print(content)
-
-
-with open ('traders.txt') as f:
-    with open ('traders.json') as f2read:
-        with open ('traders.csv') as f2write:
-
-Перевод json файл в cловарь
-import json
-
-with open ('traders.json') as f:
-    for line in f:
-        line = line.strip()
-        dict_ = json.loads(line)
-        print(dict_, type(dict_))
-
-1)with open('traders.json','r') as f:
-    for line in f:
-        line = line.strip()
-        dict_ = json.loads(line)
-        content = dict_
-
-2)with open('data) as f2write:
-    json.dump(content, f2write)
-    f2write.write('\n')
-
-3)with open('data'') as f:
-    for line in f:
-        line = line.strip()
-        dict2_ = json.loads(line)
-        content = dict2_
-
-import json
-
-with open('traders.json','r') as f:
-    for line in f:
-        line = line.strip()
-        dict_ = json.loads(line)
-        content = f1.read()
-        dict_ = json.loads(content)
-        contents = dict_
-        print(contents)
-
-import json
-from traders.json
-from traders.txt
-
-result = ['7702758341', '7802654025', '5027217264','6324042940','5834031870','1657061756','3665044042','6453102410']
-
-import json
-
-with open('traders.json','r') as f1:
-        content = f1.read()
-        data = json.loads(content)
-        print(data)
-
-with open('traders.txt') as f2:
-    for line in enumerate(f2):
-        inn_list = [line.strip() for line in f2]
-        inn = ['7802654025', '5027217264', '6324042940', '5834031870', '1657061756', '3665044042', '6453102410']
-        if inn in data:
-            inn = data[inn]
-            inn = data[ogrn]
-            inn = data[address]
-
-with open('traders.json', 'r') as f2:
-    content = f2.read()
-    data = json.loads(content)
-
-
-
 """
+
+#Task 1
+
 import json
 
-with open('traders.txt') as f1:
-    for line in enumerate(f1):
-        inn_list = [line.strip() for line in f1]
-        print(inn_list)
+inn_list = []
+org_list = []
 
+# 1. Получаем из файла список ИНН
+with open('traders.txt') as f1:
+    for line in f1:
+        inn_list.append(line.strip())
+
+# 2. Находим в json файле информацию об организации по ИНН из первого файла
 with open('traders.json', 'r') as f2:
-    for line in f2:
-        line = line.strip()
-        dict_ = json.loads(line)
-        if inn_list in dict_:
-            inn_list = dict_[inn]
-            inn_list = dict_[address]
-            inn_list = dict_[address]
+    org_data = json.load(f2)
+    for org in org_data:
+        if org['inn'] in inn_list:
+            org_list.append(org)
+
+# 3. Сохраняем информацию в новом файле
+with open('traders.csv', 'w', encoding='utf-8') as f3:
+    f3.write('INN, OGRN, ADDRESS\n')
+    for org in org_list:
+        inn = org['inn']
+        ogrn = org['ogrn']
+        address = org['address']
+        f3.write(f'{inn}, {ogrn}, {address}\n')
+
+
+
+
+
+
+
 
 
 
