bd9e20b8ecf2cef298783aa7a69cc22305a2ee63
gregorydavydov <davydovgrand@gmail.com>
2023-05-02 19:53:06 +0300

    into

--- lection4/lection 4.py
+++ lection4/lection 4.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 
 # ДЗ 7
 
@@ -10,20 +9,66 @@ class CourtCase:
         self.in_finished = in_finished
         self.verdict = verdict
 
-    def set_a_listening_datetime(self): # Добавляет в список "listening_datetimes" судебное заседание
+    def set_a_listening_datetime(self, new_time):  # Добавляет в список "listening_datetimes" судебное заседание
+        self.listening_datetimes = []
+        if new_time not in self.listening_datetimes:
+            self.listening_datetimes.append(new_time)
+
+    def add_participant(self, inn):  # Добавляет в списка "case_participants" ИНН участника
+        self.case_participants = []
+        if inn not in self.case_participants:
+            self.case_participants.append(inn)
+
+    def remove_participant(self, inn):  # Удаляет из списка "case_participants" ИНН участника
+        if inn in self.case_participants:
+            self.case_participants.remove(inn)
+
+    def make_a_decision(self,
+                        decide):  # Вынести решение по делу, добавить verdict и сменить атрибут "is_finished" на true
+        self.in_finished = False
+        verdict = ''
+        if decide != self.in_finished:
+            self.verdict = 'Решение вступило в силу'
+        else:
+            self.verdict = 'Решение не вступило в силу'
 
 
-    def add_participant(self): # Добавляет в список "case_participants" ИНН участника
+a = CourtCase(case_number='А40-183194/2015', case_participants=[], listening_datetimes=[], in_finished=False, verdict='')
+a.case_number
+a.set_a_listening_datetime('01-05-2023')
+a.listening_datetimes
+a.add_participant('862202483517')
+a.add_participant('862202483518')
+a.remove_participant('862202483518')
+a.case_participants
+a.make_a_decision(False)
+a.verdict
 
 
-    def remove_participant(self): # Удаляет из список "case_participants" ИНН участника
 
 
-    def make_a_decision(self): # Вынести решение по делу, добавить verdict и сменить атрибут "is_finished" на true
+#ДЗ 8
 
+class DistanceSell:
+  def __init__(self, list_products, period_refuse):
+    self.list_product = list_products
+    self.period_refuse = period_refuse
+  def type_products(self, type):
+    self.list_products = ['medicine','cosmetic','textile','domestic chemicals','furniture','technically complex', 'non-periodic publications', 'animals']
+    if type not in self.list_products:
+      print('Товар подлежит обмену или возврату')
+    else:
+      print('Товар не подлежит обмену или возврату')
+  def refuse_product(self, period_receipt):
+    self.period_refuse < 7
+    if period_receipt <= self.period_refuse:
+      print('Срок возврата не пропущен')
+    else:
+      print('Пропущен срок возврата')
 
-
-
+a = DistanceSell(list_products=[], period_refuse=7)
+a.type_products('shoes')
+a.refuse_product(3)
 
 
 
