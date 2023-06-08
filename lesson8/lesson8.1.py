513372c56f012bc0c8c596a339aeff874e66397f
gregorydavydov <davydovgrand@gmail.com>
2023-06-09 00:48:27 +0300

    into

--- lesson8/lesson8.1.py
+++ lesson8/lesson8.1.py
@@ -1,27 +1,36 @@
+#ДЗ 13
+
 from datetime import date
 import requests
 from bs4 import BeautifulSoup
-import re
-def today_date():
-    today = date.today().strftime("%d.%m.%Y")
-    return today
+import json
 
-def get_page():
-    url = f"https://cbr.ru/currency_base/daily/?" \
-          f"UniDbQuery.Posted=True&" \
-          f"UniDbQuery.To={today_date()}"
-    r = requests.get(url)
-    return r.text
+class ParserCBRF:
+    def __init__(self):
+        self.url = ""
+    def __today_date(self):
+        today = date.today().strftime("%d.%m.%Y")
+        return today
+    def __get_page(self):
+        url = f"https://cbr.ru/currency_base/daily/?" \
+              f"UniDbQuery.Posted=True&" \
+              f"UniDbQuery.To={self.__today_date()}"
+        r = requests.get(url)
+        return r.text
+    def __get_info(self):
+        html = self.__get_page()
+        soup = BeautifulSoup(html, "html.parser")
+        table = [i.text for i in soup.find("table", class_="data").find_all("td")]
+        self.table = table
 
-def main():
-    html = get_page()
-    soup = BeautifulSoup(html, "html.parser")
-    table = [i.text for i in soup.find("table", "data").find_all("td")]
-    print("stop")
-    currency_text = table
-    currencies = re.findall('[а-яА-ЯёЁ]+', currency_text)
-    rate = table[4::5]
-    pass
+    def __table_data(self, table_json ):
+        with open(table_json, "w") as table2write:
+            json.dump(self.table, table2write)
+    def run(self):
+        self.__get_info()
+        self.__table_data("result.json")
 
 if __name__ == "__main__":
-    main()
+    pars = ParserCBRF()
+    pars.run()
+
